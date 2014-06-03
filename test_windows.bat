45de0ebe8980016e9f9a6f1a56733c87524176e4
alex85k <alexei_kasatkin@mail.ru>
2014-06-03 18:33:19 +0600

    fix test_windows.bat (will rebase)

--- test_windows.bat
+++ test_windows.bat
@@ -1,5 +1,5 @@
 set PATH=%PATH%;x:\libs18\bin;x:\tbb\bin\intel64\vc12
-echo "disk=m:\temp\stxxl,1000,wincall" > stxxl.txt
+echo disk=m:\temp\stxxl,1000,wincall > test/stxxl.txt
 set OSRM_TIMEOUT=200
 set STXXLCFG=stxxl.txt
 cucumber > testlog.txt
