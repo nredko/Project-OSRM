Project-OSRM/osrm-backend

std::string and char*-related fixes for MSVC

extractor.cpp
@@ -120,7 +120,7 @@ int main (int argc, char *argv[]) {
         {
             SimpleLogger().Write() << "Reading options from: " << config_file_path.string();
             std::string config_str;
-            PrepareConfigFile(config_file_path.c_str(), config_str);
+            PrepareConfigFile(config_file_path, config_str);
             std::stringstream config_stream( config_str );
             boost::program_options::store(parse_config_file(config_stream, config_file_options), option_variables);
             boost::program_options::notify(option_variables);
@@ -155,7 +155,7 @@ int main (int argc, char *argv[]) {
         SimpleLogger().Write() << "Threads: " << requested_num_threads;
 
         /*** Setup Scripting Environment ***/
-        ScriptingEnvironment scriptingEnvironment(profile_path.c_str());
+        ScriptingEnvironment scriptingEnvironment(profile_path.string().c_str());
 
         omp_set_num_threads(std::min(omp_get_num_procs(), requested_num_threads));
 
@@ -207,11 +207,11 @@ int main (int argc, char *argv[]) {
         BaseParser* parser;
         if(file_has_pbf_format)
         {
-            parser = new PBFParser(input_path.c_str(), extractCallBacks, scriptingEnvironment);
+            parser = new PBFParser(input_path.string().c_str(), extractCallBacks, scriptingEnvironment);
         }
         else
         {
-            parser = new XMLParser(input_path.c_str(), extractCallBacks, scriptingEnvironment);
+            parser = new XMLParser(input_path.string().c_str(), extractCallBacks, scriptingEnvironment);
         }
 
         if(!parser->ReadHeader())

prepare.cpp
@@ -200,10 +200,10 @@ int main (int argc, char *argv[]) {
         luaL_openlibs(myLuaState);
 
         //adjust lua load path
-        luaAddScriptFolderToLoadPath( myLuaState, profile_path.c_str() );
+        luaAddScriptFolderToLoadPath( myLuaState, profile_path.string().c_str() );
 
         // Now call our function in a lua script
-        if(0 != luaL_dofile(myLuaState, profile_path.c_str() )) {
+        if(0 != luaL_dofile(myLuaState, profile_path.string().c_str() )) {
             std::cerr <<
                 lua_tostring(myLuaState,-1)   <<
                 " occured in scripting block" <<

nredko/Project-OSRM

extractor.cpp
@@ -177,7 +177,7 @@ int main(int argc, char *argv[])
         tbb::task_scheduler_init init(real_num_threads);
 
         /*** Setup Scripting Environment ***/
-        ScriptingEnvironment scripting_environment(profile_path.c_str());
+        ScriptingEnvironment scripting_environment(profile_path.string().c_str());
 
         bool file_has_pbf_format(false);
         std::string output_file_name = input_path.string();
@@ -229,11 +229,11 @@ int main(int argc, char *argv[])
         BaseParser *parser;
         if (file_has_pbf_format)
         {
-            parser = new PBFParser(input_path.c_str(), extractor_callbacks, scripting_environment);
+            parser = new PBFParser(input_path.string().c_str(), extractor_callbacks, scripting_environment);
         }
         else
         {
-            parser = new XMLParser(input_path.c_str(), extractor_callbacks, scripting_environment);
+            parser = new XMLParser(input_path.string().c_str(), extractor_callbacks, scripting_environment);
         }
 
         if (!parser->ReadHeader())

prepare.cpp
@@ -229,10 +229,10 @@ int main(int argc, char *argv[])
         luaL_openlibs(lua_state);
 
         // adjust lua load path
-        luaAddScriptFolderToLoadPath(lua_state, profile_path.c_str());
+        luaAddScriptFolderToLoadPath(lua_state, profile_path.string().c_str());
 
         // Now call our function in a lua script
-        if (0 != luaL_dofile(lua_state, profile_path.c_str()))
+        if (0 != luaL_dofile(lua_state, profile_path.string().c_str()))
         {
             std::cerr << lua_tostring(lua_state, -1) << " occured in scripting block" << std::endl;
             return 1;