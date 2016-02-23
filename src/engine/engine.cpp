3223dd3a25af34816bee118106e6a1fe04bda3d9
Nikolay Redko <nredko@nredko.com>
2016-02-24 02:08:51 +0600

    .

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -9,7 +9,7 @@
 #include "engine/plugins/trip.hpp"
 #include "engine/plugins/viaroute.hpp"
 #include "engine/plugins/match.hpp"
-#include "engine/plugins/isochrone.hpp"
+#include "engine/plugins/isochrone_plugin.hpp"
 
 #include "engine/datafacade/datafacade_base.hpp"
 #include "engine/datafacade/internal_datafacade.hpp"
